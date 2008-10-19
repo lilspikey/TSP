1668b765ceafc284a942acecf330a372229ecc15
John Montgomery <john@littlespikeyland.com>
2008-10-19 19:55:59 +0000

    got the ga getting some better results (made algorithm greedier).
    git-svn-id: svn://imac/TSP/trunk@545 3acc6705-b532-0410-8d71-db9fc28556b5

--- ga.py
+++ ga.py
@@ -32,23 +32,43 @@ class Individual(object):
     def __repr__(self):
         return "Individual(%d)" % self.score()
 
+def pop_stats(population):
+    pop_size=len(population)
+    scores=[p.score() for p in population]
+    avg=sum(scores)/float(pop_size)
+    var=sum((score-avg)**2 for score in scores)/float(pop_size)
+    print "avg: ", avg, " var: ", var
+
 def evolve(init_function,move_operator,objective_function,max_evaluations,recombine_operator):
     objective_function=ObjectiveFunction(objective_function)
     
-    pop_size=4
+    pop_size=5
+    mutation_rate=1.0
+    breed_rate=0.1
     population=[Individual(init_function(),objective_function,move_operator,recombine_operator) for i in xrange(pop_size)]
     
     while objective_function.num_evaluations < max_evaluations:
         p1,p2=random.sample(population,2)
-        p1=p1.mutate()
-        p2=p2.mutate()
-        child=p1.breed(p2)
-        population.append(child)
-        population.sort(key=Individual.score, reverse=True)
-        population=population[:pop_size]
+        #if p1.score() < p2.score():
+        #    population.remove(p1)
+        #else:
+        #    population.remove(p2)
+        if random.random() < breed_rate:
+            child=p1.breed(p2)
+        else:
+            child=p1
+        if random.random() < mutation_rate:
+            child=child.mutate()
         
-        #if objective_function.num_evaluations % 20 == 0:
-        #    print population
+        if p1.score() < child.score():
+            population.remove(p1)
+            population.append(child)
+        #population.append(child)
+        #population.sort(key=Individual.score, reverse=True)
+        #population=population[:pop_size]
+        
+        if objective_function.num_evaluations % 1000 == 0:
+            pop_stats(population)
     
     return (objective_function.num_evaluations,objective_function.best_score,objective_function.best)
 
